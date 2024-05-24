4a6cfa588bccdb49672dab2691444b29fe4f4546
Sohan Maali <sohanmaali4@gmail.com>
2024-05-24 20:15:17 +0530

    error and response add

--- src/index.js
+++ src/index.js
@@ -1,22 +1,23 @@
 // require('dotenv').config();
 import dotenv from "dotenv";
-import express from "express"
 import connectDB from "./db/index.js";
+import { app } from "./app.js";
 
-const app = express();
 dotenv.config();
 
- console.log(process.env.MONGODB_URI);
+console.log(process.env.MONGODB_URI);
 connectDB()
-  .then((result) => {
-    app.listen(process.env.PORT || 3300, () => {
-      console.log(`Server is running at port : ${process.env.PORT}`);
-    });
-    console.log("DB Connected ",result);
-  })
-  .catch((err) => {
-    console.log("MognoDB Fail to connect !!!", err);
-  });
+        .then((result) => {
+                app.listen(process.env.PORT || 3300, () => {
+                        console.log(
+                                `Server is running at port : ${process.env.PORT}`
+                        );
+                });
+                console.log("DB Connected ", result);
+        })
+        .catch((err) => {
+                console.log("MognoDB Fail to connect !!!", err);
+        });
 
 /*
 import express from "express";
